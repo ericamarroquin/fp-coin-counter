addbc3f206eb0066a641ae1f12c6c8dee14bf114
EricaMarroquin <ericamarroquin03@gmail.com>
2021-11-08 14:41:53 -0800

    add passing test for returning number of quarters
    Co-authored-by: Melissa Schatz-Miller <melissa.scmi@gmail.com>

--- __tests__/counter.test.js
+++ __tests__/counter.test.js
@@ -2,23 +2,62 @@ import {counter} from './../src/counter.js';
 
 describe ('counter', () => {
 
-  test('should correctly return inputted dollarage', () => {
-    const count = counter(6.66)(1);
-    expect(count).toEqual(6.66); 
-  });
+  // test('should correctly return inputted dollarage', () => {
+  //   const count = counter(6.66);
+  //   expect(count).toEqual("6.66 dollars"); 
+  // });
 
   test('should correctly determine if input is a number', () => {
-    const count = counter()("bloop");
+    const count = counter("bloop");
     expect(count).toEqual("WRONG INPUT! >[");
   });
 
   test('should return input if less than or equal to 0', () => {
-    const count = counter(0)(0);
-    expect(count).toEqual("Finished! Your total is $0");
+    const count = counter(0);
+    expect(count).toEqual("& none");
   });
 
-  test('should return dollarage multiplied by 100', () => {
-    const pennifier = counter(6.66)(100);
-    expect(pennifier).toEqual(666);
+  test('should return number of quarters in coinage', () => {
+    const count = counter(1.00);
+    expect(count).toEqual("4 quarters & none");
   });
+  
+  // test('should return dollarage multiplied by 10', () => {
+  //   const dollars = counter(6.66);
+  //   const dimes = dollars(10);
+  //   expect(dimes).toEqual(66.6);
+  // });
 });
+
+// should go through the currencies until you have it all in coins... >[
+  // so if we're writing a recursive loop for that, then...first it should take your dollars and multiply by 100 and then start dividing?
+  // we have 6.66, then we turn it into 666 pennies, then we divide 666/25, so we have 26 quarters and 64 pennies, then we divide by 10, so we have 26 quarters and 6 dimes and 4 pennies. and we don't divide by 5, because it's already too small.
+
+// function coin(input)
+// input * 100
+// if 
+
+// describe ('counter', () => {
+
+//   test('should correctly return inputted dollarage', () => {
+//     const count = counter(6.66)(1);
+//     expect(count).toEqual(6.66); 
+//   });
+
+//   test('should correctly determine if input is a number', () => {
+//     const count = counter()("bloop");
+//     expect(count).toEqual("WRONG INPUT! >[");
+//   });
+
+//   test('should return input if less than or equal to 0', () => {
+//     const count = counter(0)(0);
+//     expect(count).toEqual("Finished! Your total is $0");
+//   });
+
+//   test('should return dollarage multiplied by 10', () => {
+//     const dollars = counter(6.66);
+//     const dimes = dollars(10);
+//     expect(dimes).toEqual(66.6);
+
+//   });
+// }); 
--- src/counter.js
+++ src/counter.js
@@ -1,17 +1,83 @@
-export const counter = (dollarage) => {
-  return (coinage) => {
-  if (isNaN(dollarage)){
-    return "WRONG INPUT! >["
-  } 
-  if (dollarage <= 0) {
-    return "Finished! Your total is $" + dollarage; 
-  }
-  else {
-    return dollarage * coinage;
-  }
-}
-}
+// export const counter = (dollarage) => {
+//   if (isNaN(dollarage)) {
+//     return "WRONG INPUT! >[";
+//   }
+//   if (dollarage <= 0) {
+//     return "Finished! Your total is $" + dollarage;
+//   }
+//   const coinage = dollarage * 100;
+//   const quarters = coinage / 25;
+//   const quarterChange = coinage % quarters;
+//     if (quarterChange === 0) {
+//       return quarters + " quarters";
+//     } else {
+//       return counter(quarterChange)
+//       return dollarage + " dollars & " + coinage + " coins";
+//     }
+//     else if (quarterChange !== 0) {
+//       const dimes = quarterChange /10;
+//       const dimeChange = quarterChange % dimes;
+//       if (dimeChange === 0) {
+//         return dimes + " dimes";
+//       }
+//     }
+// }
 
+// export const oldCounter = (dollarage) => {
+//   return (coinage) => {
+//   if (isNaN(dollarage)){
+//     return "WRONG INPUT! >["
+//   } 
+//   if (dollarage <= 0) {
+//     return "Finished! Your total is $" + dollarage; 
+//   }
+//   else {
+//     return dollarage * coinage;
+//   }
+// }
+// }
+
+export const counter = (dollarage) => {
+  if (isNaN(dollarage)) {
+    return "WRONG INPUT! >[";
+  }
+  if (dollarage <= 0) {
+    return "& none";
+  }
+  const coinage = dollarage * 100;
+  if (coinage >= 25) {
+    const quarters = coinage / 25;
+    const newDollarage = dollarage - ((quarters * 25)/100);
+    return quarters + " quarters " + counter(newDollarage);
+  } else {
+    return "no more quarters :[";
+  }
+} 
+
+/*
+8.73
+const coinage = 8.73 * 100;
+pennies = 873
+873/25
+quarters = 34
+if (quarters % 25 === 0);
+else
+const remainder = quarters/25
+remainder = 92
+remainder/10 
+dimes = 9
+if (dimes % 25 )
+newRemainder = 2
+*/
+
+// const coinage = (cash * 100) 
+// if coinage % 25 === 0, return coinage as quarters
+// else, restart loop
+// if (remainder of coinage/25) % 10 === 0, return coinage as quarters and dimes
+// else, restart loop
+// if (remainder of (remainder of coinage/25)/10) % 5 === 0, return coinage as quarters, dimes, nickels
+// else, restart loop
+// return coinage as quarters, dimes, nickels, pennies
 
 
 
